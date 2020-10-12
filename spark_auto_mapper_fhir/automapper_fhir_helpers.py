fd0eabf00ef11a332313b7fa2338fbbea5328dd1
imranq2 <imranq2@hotmail.com>
2020-10-12 11:56:16 -0700

    update to latest sparkautomappers package

--- spark_auto_mapper_fhir/automapper_fhir_helpers.py
+++ spark_auto_mapper_fhir/automapper_fhir_helpers.py
@@ -1,5 +1,5 @@
 from spark_auto_mapper.data_types.automapper_defined_types import AutoMapperTextType
-from spark_auto_mapper.data_types.automapper_data_type_date import AutoMapperDataTypeDate
+from spark_auto_mapper.data_types.automapper_data_type_date import AutoMapperDateDataType
 from spark_auto_mapper.data_types.automapper_data_type_list import AutoMapperDataTypeList
 from spark_auto_mapper_fhir.fhir_types.automapper_fhir_data_type_human_name import AutoMapperFhirDataTypeHumanName
 from spark_auto_mapper_fhir.fhir_types.automapper_fhir_data_type_patient import AutoMapperFhirDataTypePatient
@@ -9,7 +9,7 @@ class AutoMapperFhirHelpers:
     # noinspection PyPep8Naming
     @staticmethod
     def patient(id_: AutoMapperTextType,
-                birthDate: AutoMapperDataTypeDate,
+                birthDate: AutoMapperDateDataType,
                 name: AutoMapperDataTypeList,
                 gender: AutoMapperTextType
                 ) -> AutoMapperFhirDataTypePatient:
