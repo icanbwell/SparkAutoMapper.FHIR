dba55efb16008541e41148ecd3fe7d71c9a4cf95
imranq2 <imranq2@hotmail.com>
2020-10-17 16:24:06 -0700

    handle date type better

--- spark_auto_mapper_fhir/fhir_types/identifier.py
+++ spark_auto_mapper_fhir/fhir_types/identifier.py
@@ -1,3 +1,6 @@
+# flake8: noqa
+# turning off flake8 on this file because of the circular reference
+#   Identifier includes Reference which includes Identifier
 from typing import Optional
 
 from spark_auto_mapper.data_types.complex.complex_base import AutoMapperDataTypeComplexBase
@@ -5,17 +8,13 @@ from spark_auto_mapper.data_types.complex.complex_base import AutoMapperDataType
 from spark_auto_mapper_fhir.fhir_types.code import FhirCode
 from spark_auto_mapper_fhir.fhir_types.codeableConcept import FhirCodeableConcept
 # noinspection SpellCheckingInspection
-from spark_auto_mapper_fhir.fhir_types.organization import FhirOrganization
 from spark_auto_mapper_fhir.fhir_types.period import FhirPeriod
 from spark_auto_mapper_fhir.fhir_types.string import FhirString
-
 from spark_auto_mapper_fhir.fhir_types.uri import FhirUri
 
 
 class FhirIdentifier(AutoMapperDataTypeComplexBase):
     # noinspection PyPep8Naming
-    from spark_auto_mapper_fhir.fhir_types.reference import FhirReference
-
     @classmethod
     def map(cls,
             use: Optional[FhirCode] = None,
@@ -23,7 +22,7 @@ class FhirIdentifier(AutoMapperDataTypeComplexBase):
             system: Optional[FhirUri] = None,
             value: Optional[FhirString] = None,
             period: Optional[FhirPeriod] = None,
-            assigner: Optional['FhirReference[FhirOrganization]'] = None
+            # assigner: Optional[FhirReference[FhirOrganization]] = None  # this is causing circular import
             ) -> 'FhirIdentifier':
         """
         Identifier Resource in FHIR
@@ -31,12 +30,13 @@ class FhirIdentifier(AutoMapperDataTypeComplexBase):
         An identifier intended for computation
 
 
-        :param use: usual | official | temp | secondary | old (If known) (https://hl7.org/FHIR/valueset-identifier-use.html)
+        :param use: usual | official | temp | secondary | old (If known)
+                    (https://hl7.org/FHIR/valueset-identifier-use.html)
         :param type_: Description of identifier https://hl7.org/FHIR/valueset-identifier-type.html
         :param system: 	The namespace for the identifier value
         :param value: The value that is unique
         :param period: Time period when id is/was valid for use
-        :param assigner: Organization that issued id (may be just text)
+        # :param assigner: Organization that issued id (may be just text)
         """
         return FhirIdentifier(
             use=use,
@@ -44,5 +44,5 @@ class FhirIdentifier(AutoMapperDataTypeComplexBase):
             system=system,
             value=value,
             period=period,
-            assigner=assigner
+            # assigner=assigner
         )
