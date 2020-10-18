d0db297a46da5d8c3333c56ef49d47ec84459229
imranq2 <imranq2@hotmail.com>
2020-10-18 16:36:21 -0700

    add practitioner.py

--- spark_auto_mapper_fhir/fhir_types/valuesets/ex_provider_qualification.py
+++ spark_auto_mapper_fhir/fhir_types/valuesets/ex_provider_qualification.py
@@ -1,47 +1,47 @@
 from typing import Callable, Type, Any
 
+from spark_auto_mapper_fhir.fhir_types.valuesets.FhirValueSetBase import FhirValueSetBase
 from spark_auto_mapper.type_definitions.native_types import AutoMapperNativeSimpleType
 
 from spark_auto_mapper_fhir.classproperty import genericclassproperty
 from spark_auto_mapper_fhir.fhir_types.uri import FhirUri
-from spark_auto_mapper_fhir.fhir_types.valuesets.FhirValueSetBase import FhirValueSetBase
 
 
 # noinspection PyMethodParameters
 # noinspection PyPep8Naming
-class FhirProviderQualificationCode(FhirValueSetBase):
+class FhirExProviderQualificationCode(FhirValueSetBase):
     """
-    http://hl7.org/fhir/v2/0360/2.7/index.html
+    https://hl7.org/FHIR/valueset-provider-qualification.html
     """
 
     @classmethod
     def map(cls,
             value: AutoMapperNativeSimpleType
-            ) -> 'FhirProviderQualificationCode':
+            ) -> 'FhirExProviderQualificationCode':
         return cls(value=value)
 
     # noinspection PyPep8Naming,SpellCheckingInspection
     class classproperty(object):
-        def __init__(self, f: Callable[..., 'FhirProviderQualificationCode']) -> None:
-            self.f: Callable[..., 'FhirProviderQualificationCode'] = f
+        def __init__(self, f: Callable[..., 'FhirExProviderQualificationCode']) -> None:
+            self.f: Callable[..., 'FhirExProviderQualificationCode'] = f
 
         def __get__(self, obj: Any,
-                    owner: Type['FhirProviderQualificationCode']
-                    ) -> 'FhirProviderQualificationCode':
+                    owner: Type['FhirExProviderQualificationCode']
+                    ) -> 'FhirExProviderQualificationCode':
             return self.f(owner)
 
     @classproperty
-    def NameOfYourFirstValue(cls) -> 'FhirProviderQualificationCode':
+    def NameOfYourFirstValue(cls) -> 'FhirExProviderQualificationCode':
         """
         Comment
         """
         # noinspection PyCallingNonCallable
-        return FhirProviderQualificationCode("A")
+        return FhirExProviderQualificationCode("A")
 
     @genericclassproperty
     def codeset(cls) -> FhirUri:
-        return "http://terminology.hl7.org/ValueSet/v2-2.7-030"
+        return "http://terminology.hl7.org/CodeSystem/ex-providerqualification"
 
     @genericclassproperty
     def oid(cls) -> FhirUri:
-        return ""
+        return "2.16.840.1.113883.4.642.3.570"
