0136814ebc479246f7d1648d6f5e6bc0220fb9b3
imranq2 <imranq2@hotmail.com>
2020-11-22 11:06:30 -0800

    add name to org

--- spark_auto_mapper_fhir/valuesets/snomed.py
+++ spark_auto_mapper_fhir/valuesets/snomed.py
@@ -9,40 +9,31 @@ from spark_auto_mapper_fhir.valuesets.FhirValueSetBase import FhirValueSetBase
 
 # noinspection PyMethodParameters
 # noinspection PyPep8Naming
-class PracticeSettingCode(FhirValueSetBase):
+class SnoMedCode(FhirValueSetBase):
     """
-    https://hl7.org/FHIR/valueset-c80-practice-codes.html
+    http://snomed.info/sct
     """
     def __init__(self, value: AutoMapperTextInputType):
         super().__init__(value=value)
 
     # noinspection PyPep8Naming,SpellCheckingInspection
     class classproperty(object):
-        def __init__(self, f: Callable[..., 'PracticeSettingCode']) -> None:
-            self.f: Callable[..., 'PracticeSettingCode'] = f
+        def __init__(self, f: Callable[..., 'SnoMedCode']) -> None:
+            self.f: Callable[..., 'SnoMedCode'] = f
 
-        def __get__(
-            self, obj: Any, owner: Type['PracticeSettingCode']
-        ) -> 'PracticeSettingCode':
+        def __get__(self, obj: Any, owner: Type['SnoMedCode']) -> 'SnoMedCode':
             return self.f(owner)
 
     @classproperty
-    def NameOfYourFirstValue(cls) -> 'PracticeSettingCode':
+    def NameOfYourFirstValue(cls) -> 'SnoMedCode':
         """
         Comment
         """
         # noinspection PyCallingNonCallable
-        return PracticeSettingCode("A")
+        return SnoMedCode("A")
 
     @genericclassproperty
     def codeset(cls) -> FhirUri:
-        """
-        http://nucc.org/provider-taxonomy
-        """
-        return "http://nucc.org/provider-taxonomy"
-
-    @genericclassproperty
-    def codeset_snomed(cls) -> FhirUri:
         """
         http://snomed.info/sct
         """
@@ -50,4 +41,6 @@ class PracticeSettingCode(FhirValueSetBase):
 
     @genericclassproperty
     def oid(cls) -> FhirUri:
-        return "2.16.840.1.113883.3.88.12.80.72"
+        """
+        """
+        return ""
