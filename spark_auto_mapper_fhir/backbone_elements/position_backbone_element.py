f2574d8aaf13ad7ee023549f467a5f2f1280ce89
imranq2 <imranq2@hotmail.com>
2020-11-22 07:29:10 -0800

    fix typo

--- spark_auto_mapper_fhir/backbone_elements/position_backbone_element.py
+++ spark_auto_mapper_fhir/backbone_elements/position_backbone_element.py
@@ -1,11 +1,15 @@
+from typing import Optional
+
 from spark_auto_mapper_fhir.backbone_elements.fhir_backbone_element_base import FhirBackboneElementBase
 from spark_auto_mapper_fhir.fhir_types.decimal import FhirDecimal
 
 
 class PositionBackboneElement(FhirBackboneElementBase):
     def __init__(
-        self, longitude: FhirDecimal, latitude: FhirDecimal,
-        altitude: FhirDecimal
+        self,
+        longitude: FhirDecimal,
+        latitude: FhirDecimal,
+        altitude: Optional[FhirDecimal] = None
     ) -> None:
         """
         PositionBackboneElement Backbone Element in FHIR
@@ -17,5 +21,5 @@ class PositionBackboneElement(FhirBackboneElementBase):
         :param altitude: Altitude with WGS84 datum
         """
         super().__init__(
-            longitude=longitude, latitude=longitude, altitude=altitude
+            longitude=longitude, latitude=latitude, altitude=altitude
         )
