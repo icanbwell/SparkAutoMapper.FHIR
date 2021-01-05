b5c2de9682423d5946c3407fad5d0cd94846ee7f
imranq2 <imranq2@hotmail.com>
2021-01-05 14:31:32 -0800

    add current_column

--- spark_auto_mapper_fhir/valuesets/FhirValueSetBase.py
+++ spark_auto_mapper_fhir/valuesets/FhirValueSetBase.py
@@ -1,4 +1,5 @@
 from datetime import date, datetime
+from typing import Optional
 
 from pyspark.sql import DataFrame, Column
 from pyspark.sql.functions import lit
@@ -11,14 +12,18 @@ class FhirValueSetBase(AutoMapperDataTypeBase):
         super().__init__()
         self.value: AutoMapperTextInputType = value
 
-    def get_column_spec(self, source_df: DataFrame) -> Column:
+    def get_column_spec(
+        self, source_df: DataFrame, current_column: Optional[Column]
+    ) -> Column:
         if isinstance(self.value, str) or isinstance(self.value, int) \
                 or isinstance(self.value, float) or isinstance(self.value, date) \
                 or isinstance(self.value, datetime):
             return lit(self.value)
 
         if isinstance(self.value, AutoMapperDataTypeBase):
-            return self.value.get_column_spec(source_df=source_df)
+            return self.value.get_column_spec(
+                source_df=source_df, current_column=current_column
+            )
 
         raise ValueError(
             f"value: {self.value} is not str or AutoMapperDataTypeBase"
