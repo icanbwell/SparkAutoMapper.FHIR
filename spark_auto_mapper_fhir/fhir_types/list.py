c2eaeff62197279a48cc569622c04cd2b41f5963
imranq2 <imranq2@hotmail.com>
2021-01-06 17:08:22 -0800

    update package

--- spark_auto_mapper_fhir/fhir_types/list.py
+++ spark_auto_mapper_fhir/fhir_types/list.py
@@ -1,6 +1,6 @@
 from typing import TypeVar, Union, Optional, List
 
-from spark_auto_mapper.data_types.array_base import AutoMapperArrayBase
+from spark_auto_mapper.data_types.array_base import AutoMapperArrayLikeBase
 from spark_auto_mapper.data_types.data_type_base import AutoMapperDataTypeBase
 from spark_auto_mapper.data_types.list import AutoMapperList
 from spark_auto_mapper.type_definitions.native_types import AutoMapperNativeSimpleType
@@ -14,7 +14,7 @@ _T = TypeVar(
 class FhirList(AutoMapperList[_T]):
     def __init__(
         self,
-        value: Optional[Union[List[_T], AutoMapperArrayBase]],
+        value: Optional[Union[List[_T], AutoMapperArrayLikeBase]],
         include_null_properties: bool = False
     ) -> None:
         super().__init__(
