32fc7e260f9c8019c9b0d8166053f622685a2f31
Amin <amin.rashidi@gmail.com>
2021-01-08 12:22:48 -0600

    adding more properties to Condition resource

--- spark_auto_mapper_fhir/resources/condition.py
+++ spark_auto_mapper_fhir/resources/condition.py
@@ -3,11 +3,14 @@ from typing import Optional
 from pyspark.sql.types import StructType
 from spark_fhir_schemas.r4.resources.condition import ConditionSchema
 
+from spark_auto_mapper_fhir.complex_types.codeableConcept import CodeableConcept
+from spark_auto_mapper_fhir.complex_types.identifier import Identifier
 from spark_auto_mapper_fhir.complex_types.meta import Meta
 from spark_auto_mapper_fhir.extensions.extension_base import ExtensionBase
 from spark_auto_mapper_fhir.fhir_types.id import FhirId
 from spark_auto_mapper_fhir.fhir_types.list import FhirList
 from spark_auto_mapper_fhir.resources.fhir_resource_base import FhirResourceBase
+from spark_auto_mapper_fhir.valuesets.condition import ConditionCode
 
 
 class Condition(FhirResourceBase):
@@ -15,7 +18,9 @@ class Condition(FhirResourceBase):
         self,
         id_: FhirId,
         meta: Optional[Meta] = None,
-        extension: Optional[FhirList[ExtensionBase]] = None
+        identifier: Optional[FhirList[Identifier]] = None,
+        code: Optional[CodeableConcept[ConditionCode]] = None,
+        extension: Optional[FhirList[ExtensionBase]] = None,
     ) -> None:
         """
         Condition Resource in FHIR
@@ -25,7 +30,12 @@ class Condition(FhirResourceBase):
         :param id_: id of resource
         """
         super().__init__(
-            resourceType="Condition", id_=id_, meta=meta, extension=extension
+            resourceType="Condition",
+            id_=id_,
+            meta=meta,
+            identifier=identifier,
+            code=code,
+            extension=extension,
         )
 
     def get_schema(self, include_extension: bool) -> Optional[StructType]:
