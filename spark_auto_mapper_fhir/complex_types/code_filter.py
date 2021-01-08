b32b58f2303259880df0750c702dc370eb47c67b
Amin <amin.rashidi@gmail.com>
2021-01-07 18:24:51 -0600

    adding the resource and types for medstar simple triage

---
+++ spark_auto_mapper_fhir/complex_types/code_filter.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+from spark_auto_mapper_fhir.complex_types.coding import Coding
+from spark_auto_mapper_fhir.extensions.extension_base import ExtensionBase
+from spark_auto_mapper_fhir.fhir_types.id import FhirId
+from spark_auto_mapper_fhir.fhir_types.list import FhirList
+from spark_auto_mapper_fhir.complex_types.fhir_complex_type_base import FhirComplexTypeBase
+from spark_auto_mapper_fhir.fhir_types.string import FhirString
+
+
+class CodeFilter(FhirComplexTypeBase):
+    def __init__(self, id_: Optional[FhirId] = None,
+                 path: Optional[FhirString] = None,
+                 search_param: Optional[FhirString] = None,
+                 code: Optional[FhirList[Coding]] = None,
+                 extension: Optional[FhirList[ExtensionBase]] = None) -> None:
+        """
+        CodeFilter Complex Type in FHIR
+        https://www.hl7.org/fhir/element.html
+        """
+        super().__init__(
+            id_=id_,
+            path=path,
+            search_param=search_param,
+            code=code,
+            extension=extension
+        )
