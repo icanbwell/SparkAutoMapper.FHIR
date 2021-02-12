aca744540793423a2916904878d2e6ecc25f3856
Abbey <65682585+abbey-icanbwell@users.noreply.github.com>
2021-02-12 14:13:04 -0600

    add valuesets

--- spark_auto_mapper_fhir/valuesets/claim_information_category.py
+++ spark_auto_mapper_fhir/valuesets/claim_information_category.py
@@ -29,12 +29,40 @@ class ClaimInformationCategoryCode(FhirValueSetBase):
             return self.f(owner)
 
     @classproperty
-    def NameOfYourFirstValue(cls) -> 'ClaimInformationCategoryCode':
+    def Information(cls) -> 'ClaimInformationCategoryCode':
         """
-        Comment
+        Codes conveying additional situation and condition information.
         """
         # noinspection PyCallingNonCallable
-        return ClaimInformationCategoryCode("A")
+        return ClaimInformationCategoryCode("info")
+
+    def Discharge(cls) -> 'ClaimInformationCategoryCode':
+        """
+        Discharge status and discharge to locations.
+        """
+        # noinspection PyCallingNonCallable
+        return ClaimInformationCategoryCode("discharge")
+
+    def RelatedServices(cls) -> 'ClaimInformationCategoryCode':
+        """
+        Nature and date of the related event e.g. Last exam, service, X-ray etc.
+        """
+        # noinspection PyCallingNonCallable
+        return ClaimInformationCategoryCode("related")
+
+    def Exception(cls) -> 'ClaimInformationCategoryCode':
+        """
+        Insurance policy exceptions.
+        """
+        # noinspection PyCallingNonCallable
+        return ClaimInformationCategoryCode("exception")
+
+    def Other(cls) -> 'ClaimInformationCategoryCode':
+        """
+        Other information identified by the type.system.
+        """
+        # noinspection PyCallingNonCallable
+        return ClaimInformationCategoryCode("other")
 
     @genericclassproperty
     def codeset(cls) -> FhirUri:
