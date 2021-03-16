4bc636da22bb117daf0248dc11cad8530de7dc90
imranq2 <imranq2@hotmail.com>
2021-03-15 17:09:04 -0700

    add care team

---
+++ spark_auto_mapper_fhir/backbone_elements/care_team_participant_backbone_element.py
@@ -0,0 +1,52 @@
+from typing import Optional, Union, TYPE_CHECKING
+
+from spark_auto_mapper_fhir.complex_types.codeableConcept import CodeableConcept
+from spark_auto_mapper_fhir.complex_types.period import Period
+from spark_auto_mapper_fhir.complex_types.reference import Reference
+from spark_auto_mapper_fhir.extensions.extension_base import ExtensionBase
+from spark_auto_mapper_fhir.fhir_types.id import FhirId
+from spark_auto_mapper_fhir.fhir_types.list import FhirList
+from spark_auto_mapper_fhir.backbone_elements.fhir_backbone_element_base import FhirBackboneElementBase
+
+if TYPE_CHECKING:
+    from spark_auto_mapper_fhir.resources.care_team import CareTeam
+from spark_auto_mapper_fhir.resources.organization import Organization
+from spark_auto_mapper_fhir.resources.patient import Patient
+from spark_auto_mapper_fhir.resources.practitioner import Practitioner
+from spark_auto_mapper_fhir.resources.practitioner_role import PractitionerRole
+from spark_auto_mapper_fhir.resources.related_person import RelatedPerson
+from spark_auto_mapper_fhir.valuesets.participant_role import ParticipantRoleCode
+
+
+class CareTeamParticipantBackboneElement(FhirBackboneElementBase):
+    # noinspection PyPep8Naming
+    def __init__(
+        self,
+        id_: Optional[FhirId] = None,
+        extension: Optional[FhirList[ExtensionBase]] = None,
+        role: Optional[FhirList[CodeableConcept[ParticipantRoleCode]]] = None,
+        member: Optional[Reference[Union[Practitioner, PractitionerRole,
+                                         RelatedPerson, Patient, Organization,
+                                         'CareTeam']]] = None,
+        onBehalfOf: Optional[Reference[Organization]] = None,
+        period: Optional[Period] = None
+    ) -> None:
+        """
+        CareTeamParticipantBackboneElement Backbone Element in FHIR
+        IMPORTANT: To comply with the Spark mapping mechanism, please make sure you add all existing FHIR
+                    properties not just the ones you need
+        https://www.hl7.org/fhir/careteam-definitions.html#CareTeam.participant
+
+        :param role: Type of involvement
+        :param member: Who is involved
+        :param onBehalfOf: Organization of the practitioner
+        :param period: Time period of participant
+        """
+        super().__init__(
+            id_=id_,
+            extension=extension,
+            role=role,
+            member=member,
+            onBehalfOf=onBehalfOf,
+            period=period
+        )
---
+++ spark_auto_mapper_fhir/valuesets/care_team_status.py
@@ -0,0 +1,83 @@
+from typing import Callable, Type, Any
+
+from spark_auto_mapper.type_definitions.defined_types import AutoMapperTextInputType
+
+from spark_auto_mapper_fhir.classproperty import genericclassproperty
+from spark_auto_mapper_fhir.fhir_types.uri import FhirUri
+from spark_auto_mapper_fhir.valuesets.FhirValueSetBase import FhirValueSetBase
+
+
+# noinspection PyMethodParameters
+# noinspection PyPep8Naming
+class CareTeamStatusCode(FhirValueSetBase):
+    """
+    IMPORTANT: To comply with the Spark mapping mechanism, please make sure you add all existing FHIR
+                properties not just the ones you need
+    https://www.hl7.org/fhir/valueset-care-team-status.html
+    """
+    def __init__(self, value: AutoMapperTextInputType):
+        super().__init__(value=value)
+
+    # noinspection PyPep8Naming,SpellCheckingInspection
+    class classproperty(object):
+        def __init__(self, f: Callable[..., 'CareTeamStatusCode']) -> None:
+            self.f: Callable[..., 'CareTeamStatusCode'] = f
+
+        def __get__(
+            self, obj: Any, owner: Type['CareTeamStatusCode']
+        ) -> 'CareTeamStatusCode':
+            return self.f(owner)
+
+    @classproperty
+    def Proposed(cls) -> 'CareTeamStatusCode':
+        """
+        The care team has been drafted and proposed, but not yet participating in the coordination and delivery of patient care.
+        """
+        # noinspection PyCallingNonCallable
+        return CareTeamStatusCode("proposed")
+
+    @classproperty
+    def Active(cls) -> 'CareTeamStatusCode':
+        """
+        The care team is currently participating in the coordination and delivery of care.
+        """
+        # noinspection PyCallingNonCallable
+        return CareTeamStatusCode("active")
+
+    @classproperty
+    def Suspended(cls) -> 'CareTeamStatusCode':
+        """
+        The care team is temporarily on hold or suspended and not participating in the coordination and delivery of care.
+        """
+        # noinspection PyCallingNonCallable
+        return CareTeamStatusCode("suspended")
+
+    @classproperty
+    def Inactive(cls) -> 'CareTeamStatusCode':
+        """
+        The care team was, but is no longer, participating in the coordination and delivery of care.
+        """
+        # noinspection PyCallingNonCallable
+        return CareTeamStatusCode("inactive")
+
+    @classproperty
+    def EnteredInError(cls) -> 'CareTeamStatusCode':
+        """
+        Comment
+        """
+        # noinspection PyCallingNonCallable
+        return CareTeamStatusCode("entered-in-error")
+
+    @genericclassproperty
+    def codeset(cls) -> FhirUri:
+        """
+        http://hl7.org/fhir/care-team-status
+        """
+        return "http://hl7.org/fhir/care-team-status"
+
+    @genericclassproperty
+    def oid(cls) -> FhirUri:
+        """
+        2.16.840.1.113883.4.642.3.153
+        """
+        return "2.16.840.1.113883.4.642.3.153"
