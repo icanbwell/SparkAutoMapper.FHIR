d1f870c1a1b81c2600c8fa129953b19cf1d468b5
Qasem AbuSalameh <78555313+qabusalameh@users.noreply.github.com>
2021-06-01 16:51:24 -0400

    adding 4 new custom extensions

---
+++ spark_auto_mapper_fhir/extensions/custom/patient_birthplace.py
@@ -0,0 +1,28 @@
+from typing import Optional
+
+from spark_auto_mapper_fhir.classproperty import genericclassproperty
+from spark_auto_mapper_fhir.complex_types.address import Address
+from spark_auto_mapper_fhir.extensions.extension_base import ExtensionBase
+from spark_auto_mapper_fhir.fhir_types.uri import FhirUri
+from spark_auto_mapper_fhir.valuesets.FhirValueSetBase import FhirValueSetBase
+
+
+class PatientBirthPlace(ExtensionBase):
+    # noinspection PyPep8Naming
+    def __init__(
+        self,
+        url: Optional[FhirUri] = None,
+        valueAddress: Optional[Address] = None
+    ) -> None:
+        """
+        Extension type in FHIR
+        """
+        super().__init__(
+            url=url or PatientBirthPlace.codeset,
+            valueAddress=valueAddress
+        )
+
+    # noinspection PyMethodParameters
+    @genericclassproperty
+    def codeset(cls) -> FhirUri:
+        return "http://hl7.org/fhir/StructureDefinition/patient-birthPlace"
---
+++ spark_auto_mapper_fhir/extensions/custom/patient_mothersmaidenname.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+from spark_auto_mapper_fhir.classproperty import genericclassproperty
+from spark_auto_mapper_fhir.extensions.extension_base import ExtensionBase
+from spark_auto_mapper_fhir.fhir_types.string import FhirString
+from spark_auto_mapper_fhir.fhir_types.uri import FhirUri
+from spark_auto_mapper_fhir.valuesets.FhirValueSetBase import FhirValueSetBase
+
+
+class PatientMothersMaidenName(ExtensionBase):
+    # noinspection PyPep8Naming
+    def __init__(
+        self,
+        url: Optional[FhirUri] = None,
+        valueString: Optional[FhirString] = None
+    ) -> None:
+        """
+        Extension type in FHIR
+        """
+        super().__init__(url=url or PatientMothersMaidenName.codeset, valueString=valueString)
+
+    # noinspection PyMethodParameters
+    @genericclassproperty
+    def codeset(cls) -> FhirUri:
+        return "http://hl7.org/fhir/StructureDefinition/patient-mothersMaidenName"
