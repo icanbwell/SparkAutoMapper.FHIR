9552ecbb46c5ccd67601eca6cfc3edb296afb308
Amin <amin.rashidi@gmail.com>
2021-06-03 16:42:30 -0500

    fix filter

--- tests/resources/patient/test_automapper_fhir_patient_resource_include_null_properties.py
+++ tests/resources/patient/test_automapper_fhir_patient_resource_include_null_properties.py
@@ -9,7 +9,6 @@ from pyspark.sql.functions import lit, struct, array, coalesce, to_date
 from spark_auto_mapper.automappers.automapper import AutoMapper
 from spark_auto_mapper.helpers.automapper_helpers import AutoMapperHelpers as A
 
-
 from spark_auto_mapper_fhir.complex_types.human_name import HumanName
 from spark_auto_mapper_fhir.fhir_types.id import FhirId
 from spark_auto_mapper_fhir.fhir_types.list import FhirList
