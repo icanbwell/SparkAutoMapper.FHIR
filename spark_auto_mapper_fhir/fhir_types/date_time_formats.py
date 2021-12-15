39416674256caacede45779879071ddd248944f5
imranq2 <imranq2@hotmail.com>
2021-12-15 10:49:41 -0800

    make date as reserved keyword

--- spark_auto_mapper_fhir/fhir_types/date_time_formats.py
+++ spark_auto_mapper_fhir/fhir_types/date_time_formats.py
@@ -1,5 +1,7 @@
 class FhirDateTimeFormat:
     date_format: str = "yyyy-MM-dd"
     date_time_format: str = "yyyy-MM-dd'T'kk:mm:ss'Z'"
+    date_time_format_no_milliseconds: str = "yyyy-MM-dd'T'hh:mm:ss'Z'"
     time_format: str = "kk:mm:ss"
+    time_format_no_milliseconds: str = "hh:mm:ss"
     instant_format: str = "yyyy-MM-dd'T'hh:mm:ss'Z'"
