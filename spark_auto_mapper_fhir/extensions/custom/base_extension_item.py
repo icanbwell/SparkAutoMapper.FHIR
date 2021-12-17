31c5483e3bc7c86b465a17cbb146ea7e9849e398
kyle paul <kyle@icanbwell.com>
2021-12-17 11:37:07 -0800

    set defaults for required properties

--- spark_auto_mapper_fhir/extensions/custom/base_extension_item.py
+++ spark_auto_mapper_fhir/extensions/custom/base_extension_item.py
@@ -1,18 +1,59 @@
 from typing import Optional
 
 from spark_auto_mapper_fhir.extensions.extension_base import ExtensionBase
+from spark_auto_mapper_fhir.fhir_types.boolean import FhirBoolean
+from spark_auto_mapper_fhir.fhir_types.date import FhirDate
 from spark_auto_mapper_fhir.fhir_types.date_time import FhirDateTime
+from spark_auto_mapper_fhir.fhir_types.decimal import FhirDecimal
+from spark_auto_mapper_fhir.fhir_types.id import FhirId
+from spark_auto_mapper_fhir.fhir_types.integer import FhirInteger
+from spark_auto_mapper_fhir.fhir_types.positive_int import FhirPositiveInt
 from spark_auto_mapper_fhir.fhir_types.string import FhirString
+from spark_auto_mapper_fhir.fhir_types.time import FhirTime
+from spark_auto_mapper_fhir.fhir_types.unsigned_int import FhirUnsignedInt
+from spark_auto_mapper_fhir.fhir_types.uri import FhirUri
+from spark_auto_mapper_fhir.fhir_types.url import FhirUrl
+from spark_auto_mapper_fhir.value_sets.generic_type import GenericTypeCode
 
 
 class BaseExtensionItem(ExtensionBase):
     # noinspection PyPep8Naming
     def __init__(
         self,
-        url: str,
-        valueString: Optional[FhirString] = None,
+        *,
+        id_: Optional[FhirString] = None,
+        url: Optional[FhirUri] = None,
+        valueBoolean: Optional[FhirBoolean] = None,
+        valueCode: Optional[GenericTypeCode] = None,
+        valueDate: Optional[FhirDate] = None,
         valueDateTime: Optional[FhirDateTime] = None,
+        valueDecimal: Optional[FhirDecimal] = None,
+        valueId: Optional[FhirId] = None,
+        valueInteger: Optional[FhirInteger] = None,
+        valuePositiveInt: Optional[FhirPositiveInt] = None,
+        valueString: Optional[FhirString] = None,
+        valueTime: Optional[FhirTime] = None,
+        valueUnsignedInt: Optional[FhirUnsignedInt] = None,
+        valueUri: Optional[FhirUri] = None,
+        valueUrl: Optional[FhirUrl] = None,
         include_null_properties: bool = True,
     ) -> None:
-        super().__init__(url=url, valueString=valueString, valueDateTime=valueDateTime)
-        super().include_null_properties(include_null_properties=True)
+        super().__init__(
+            id_=id_,
+            extension=None,
+            url=url,
+            valueBoolean=valueBoolean,
+            valueCode=valueCode,
+            valueDate=valueDate,
+            valueDateTime=valueDateTime,
+            valueDecimal=valueDecimal,
+            valueId=valueId,
+            valueInteger=valueInteger,
+            valuePositiveInt=valuePositiveInt,
+            valueString=valueString,
+            valueTime=valueTime,
+            valueUnsignedInt=valueUnsignedInt,
+            valueUri=valueUri,
+            valueUrl=valueUrl,
+        )
+        super().include_null_properties(include_null_properties=include_null_properties)
