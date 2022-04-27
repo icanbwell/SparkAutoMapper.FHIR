96da9cf316f89c5c2734232272b575f5381afd5e
kyle <djjoebanks@gmail.com>
2022-04-27 10:32:42 -0700

    update spark auto mapper to 1.0.12 (#122)
    Co-authored-by: kyle paul <kyle@icanbwell.com>

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
     install_requires=[
         "logger",
         "pyspark>=3.1.1",
-        "sparkautomapper>=1.0.10",
+        "sparkautomapper>=1.0.12",
         "sparkfhirschemas>=1.0.6",
         "deprecated",
     ],
